164207f17343b9544ccfd75176579eb2a210eeb8
Barnet Obeka <oworibarnet@gmail.com>
2023-04-18 18:09:59 +0100

    added a CORS for acces to the server

--- express/src/colors.mjs
+++ express/src/colors.mjs
@@ -15,16 +15,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes
 app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.send('Colour Library');
 });
 
 //List all Colours
 app.get('/colours', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.send(colours);
 });
 
 //Select a Colour with specific ColorId
 app.get('/colours/:id', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   const id = parseInt(req.params.id);
   const colors = colours.find((colours) => colours.colorId === id);
   res.send(colors);
@@ -32,31 +35,34 @@ app.get('/colours/:id', (req, res) => {
 
 //Create a new color
 app.post('/colours', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   const hexString = req.body.hexString;
   const rgb = req.body.rgb;
   const hsl = req.body.hsl;
   const name = req.body.name;
   const colorId = books.length + 1;
   books.push({ colorId: colorId, hexString: hexString, rgb: rgb, hsl: hsl, name:name });
-  res.send('Colour Created');
+  res.send({"created":'Colour Created'});
 });
 
 //Edit an existing color
 app.post('/colours/:id/edit', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   const id = parseInt(req.params.id);
   const color = colours.find((color) => color.colorId === id);
   color.hexString = req.body.hexString;
   color.rgb = req.body.rgb;
   color.hsl = req.body.hsl;
   color.name = req.body.name;
-  res.send('Colour Edited');
+  res.send({"edited":'Colour Edited'});
 });
 
 //Delete an existing Colour
 app.get('/colours/:id/delete', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   const id = parseInt(req.params.id);
   colours = colours.filter((colours) => colours.colorId !== id);
-  res.send('Colour Deleted');
+  res.send({"deleted":'Colour Deleted'});
 });
 
 // Configure template engine
