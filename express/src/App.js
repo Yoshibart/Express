cb80a6ee78ee8e5a32e0454f106e3e28f644d2a1
Barnet Obeka <oworibarnet@gmail.com>
2023-04-18 18:10:48 +0100

    impleted the nextButton, previousButton and rrmove color fucntionality

--- express/src/App.js
+++ express/src/App.js
@@ -1,50 +1,107 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/fontawesome-free-solid'
-function App() {
+import { useState, useEffect} from 'react';
+
+export default function App() {
+  const [allColors, setAllColors] = useState([]);
+  const [color, setColor] = useState({colorId:"",hexString:"",rgb:"",hsl:"",name:""});
+
+  useEffect(() => {
+    fetch(`http://localhost:3030/colours/`).then(response => response.json())
+    .then(datas => {
+      setAllColors(datas);
+    });    
+  }, [color]);
+
+  const setColorInputs = event =>{
+    setColor(oldata =>{return {...oldata,[event.target.name]:event.target.value}})}
+
+  const deleteColor = () => {
+    fetch(`http://localhost:3030/colours/${color.colorId}/delete`).then(response => response.json())
+    .then(data => {
+      console.log(data.deleted)
+    });
+    nextColor();
+  }
+
+  const previousColor = () => {
+    let data;
+    for(let colors of allColors){
+      if(colors.colorId == color.colorId) break;
+      data = colors
+    }
+    const { r, g, b } = data.rgb;
+    const h = Number(data.hsl.h) > 0 ? `${data.hsl.h}%` : data.hsl.h;
+    const s = Number(data.hsl.s) > 0 ? `${data.hsl.s}%` : data.hsl.s;
+    const l = Number(data.hsl.l) > 0 ? `${data.hsl.l}%` : data.hsl.l;
+    data.rgb = `rgb(${r},${g},${b})`;
+    data.hsl = `hsl(${h},${s},${l})`;
+    setColor(data);
+  };
+
+  const nextColor = () => {
+    let data;
+    for(let colors = allColors.length-1; colors > -1; colors--){
+      if(allColors[colors].colorId == color.colorId) break;
+      data = allColors[colors]
+    }
+    const { r, g, b } = data.rgb;
+    const h = Number(data.hsl.h) > 0 ? `${data.hsl.h}%` : data.hsl.h;
+    const s = Number(data.hsl.s) > 0 ? `${data.hsl.s}%` : data.hsl.s;
+    const l = Number(data.hsl.l) > 0 ? `${data.hsl.l}%` : data.hsl.l;
+    data.rgb = `rgb(${r},${g},${b})`;
+    data.hsl = `hsl(${h},${s},${l})`;
+    setColor(data);
+  }
+
+  const showColor = ()=>{
+    let showButtonSelection = document.getElementById("show");
+    showButtonSelection.hidden = !showButtonSelection.hidden;
+  }
 
   return (
     <>
       <div id="option_buttons">
-        <button>Show Color</button><br/>
-        <button>Insert Color</button><br/>
-        <button>Remove Color</button><br/>
-        <button>Modify Color</button><br/>
-        <button>Select Background</button><br/>
+        <button onClick={showColor}>Show Color</button><br/>
+        <button >Insert Color</button><br/>
+        <button onClick={deleteColor}>Remove Color</button><br/>
+        <button id="modify">Modify Color</button><br/>
+        <button id="background">Select Background</button><br/>
       </div>
       <div id="inputs-section">
         <div id="input">
           <p>
             <label>ColorId:</label>
-            <input name="ColorId" required />
+            <input name="colorId" required onChange={setColorInputs} value={color.colorId}/>
           </p>
           <p>
             <label>HexString:</label>
-            <input name="HexString" required />     
+            <input name="hexString" required onChange={setColorInputs} value={color.hexString}/>     
           </p>
           <p>
             <label>RGB:</label>
-            <input name="RGB" required />
+            <input name="rgb" required onChange={setColorInputs}value={color.rgb}/>
           </p>
           <p>
             <label>HSL:</label>
-            <input name="HSL" required />
+            <input name="hsl" required onChange={setColorInputs} value={color.hsl}/>
           </p>
           <p>
             <label>Name:</label>
-            <input name="Name" required />       
+            <input name="name" required onChange={setColorInputs} value={color.name}/>       
           </p>
           <p>
-            <button><FontAwesomeIcon icon={faAngleLeft} /></button>
-            <button><FontAwesomeIcon icon={faAngleRight} /></button>
+            <button onClick={previousColor}><FontAwesomeIcon icon={faAngleLeft} /></button>
+            <label>{color.colorId}</label>
+            <button onClick={nextColor}><FontAwesomeIcon icon={faAngleRight} /></button>
           </p>
         </div>
-        <div  id="display" >
+        <div id="display" >
           <label>Display Color:</label>
-          <p style={{width: "200px", height:"200px",backgroundColor:"red"}}></p>    
+          <p id="show" style={{width: "200px", height:"200px",backgroundColor:color.hexString}} hidden></p>    
         </div>
       </div>
     </>
   );
 }
-export default App;
